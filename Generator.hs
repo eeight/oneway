f98feb8453f99a82d7cfe95d5479ddf7814fa2d7
Petr Prokhorenkov <eeight@yandex-team.ru>
2012-10-31 15:18:05 +0400

    Don't generate uneeded methods for non-toplevel classes.

--- Generator.hs
+++ Generator.hs
@@ -39,7 +39,7 @@ generateCxx automata name = let
     states = viableStates automata
     inStates = incomingStates automata
 
-    generateTemplate automata name indent = let
+    generateTemplate automata name indent toplevel = let
         doPut level str = putStr (concat $ replicate level "  ") >> putStr str
         put = doPut indent
         put' = doPut (indent + 1)
@@ -56,12 +56,13 @@ generateCxx automata name = let
         maybePutText''' = doMaybePutText (indent + 3)
 
         generateIncoming state = let
-            fromStates = (state, B.empty):maybe [] id (M.lookup state inStates)
-            fromStates' = filter ((`S.member` states) . fst) fromStates
-            fromStates'' = groupBy ((==) `on` snd) $
-                    sortBy (compare `on` snd) fromStates'
+            fromStates =
+                    groupBy ((==) `on` snd) $
+                    sortBy (compare `on` snd) $
+                    filter ((`S.member` states) . fst) $
+                    (state, B.empty):maybe [] id (M.lookup state inStates)
             fromGroups = map (\((i, str):ss) -> (sort $ i:map fst ss, str))
-                    fromStates''
+                    fromStates
             merger = stringMerger $ map snd fromGroups
             constants = stringConstants merger
             in do
@@ -98,7 +99,7 @@ generateCxx automata name = let
             put' "}\n"
 
         generatePiece (Block state name body:_) = do
-            generateTemplate body name (indent + 1)
+            generateTemplate body name (indent + 1) False
             let className = makeClassName name
             put' $ printf "%s *add%s() {\n" className className
             generateIncoming state
@@ -114,31 +115,32 @@ generateCxx automata name = let
             let className = makeClassName name
             put $ printf "class %s : private AbstractTemplate {\n" className
             put "public:\n"
-            put' $ printf "%s() {\n" className
-            put'' "construct();\n"
-            put' $ "}\n"
-
             put' "void construct() {\n"
             maybePutText'' carry
             put'' $ printf "state_ = %d;\n" realBegin
             put' "}\n"
 
-            put' "const char* data() const { return getData(); }\n"
-            put' "size_t size() const { return getSize(); }\n"
+            when toplevel $ do
+                put' $ printf "%s() {\n" className
+                put'' "construct();\n"
+                put' $ "}\n"
+
+                put' $ printf "~%s() {\n" className
+                put'' "finalize();\n"
+                put' "}\n"
+
+                put' $ printf "void finalize() {\n"
+                generateIncoming (finalState automata)
+                put' "}\n"
+
+                put' "const char* data() const { return getData(); }\n"
+                put' "size_t size() const { return getSize(); }\n"
 
             forM_ (tails automata) generatePiece
-
-            put' $ printf "void finalize() {\n"
-            generateIncoming (finalState automata)
-            put' "}\n"
-
-            put' $ printf "~%s() {\n" className
-            put'' "finalize();\n"
-            put' "}\n"
 
             put "};\n"
 
     in do
         putStrLn "#include \"abstract_template.h\""
         putStrLn ""
-        generateTemplate automata name 0
+        generateTemplate automata name 0 True
